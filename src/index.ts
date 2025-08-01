58f8db6e16888cad27fd0b4dc78ff5d1a69dbf26
jeanpaul84 <jean.chacongonzalez@ucr.ac.cr>
2025-08-01 17:17:13 -0600

    add StopTimesFetcher as a component in the index typescript

--- src/index.ts
+++ src/index.ts
@@ -4,6 +4,7 @@ import InfobusRouteMap from './components/InfobusRouteMap.vue'
 import { useInfobusApi } from './composables/useInfobusApi'
 import { useNextTrips } from './composables/useNextTrips'
 import { useRouteShapes } from './composables/useRouteShapes'
+import StopTimesFetcher from './components/StopTimesFetcher.vue'
 
 export type {
   NextTrip,
@@ -25,5 +26,6 @@ export default {
   install(app: App) {
     app.component('InfobusNextTrips', InfobusNextTrips)
     app.component('InfobusRouteMap', InfobusRouteMap)
+    app.component('StopTimesFetcher', StopTimesFetcher)
   }
 }
