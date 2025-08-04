ca78c8f0f954ff497b78607e2068504822f6edf8
jeanpaul84 <jean.chacongonzalez@ucr.ac.cr>
2025-08-04 11:04:21 -0600

    refactor logic in temporal stop times component

--- src/components/StopTimesFetcher.vue
+++ src/components/StopTimesFetcher.vue
@@ -45,69 +45,23 @@
 
 <script setup lang="ts">
 import { ref } from 'vue'
+import { useStopTimes } from '../composables/useStopTimes'
 
 const tripId = ref('')
 const output = ref('')
 const tableData = ref<any[]>([])
 
-// Mocked API response
-const mockStopTimes = [
-  {
-    trip_id: "JFH367",
-    arrival_time: "07:15:00",
-    departure_time: "07:15:00",
-    stop_id: "bUCR-0-03",
-    stop_sequence: 15,
-    stop_headsign: "Facultad de Ingeniería",
-    pickup_type: 0,
-    drop_off_type: 0,
-    shape_dist_traveled: 0.5
-  },
-  {
-    trip_id: "JFH367",
-    arrival_time: "07:20:00",
-    departure_time: "07:20:00",
-    stop_id: "bUCR-0-04",
-    stop_sequence: 16,
-    stop_headsign: "Facultad de Ingeniería",
-    pickup_type: 0,
-    drop_off_type: 0,
-    shape_dist_traveled: 1.0
-  },
-  {
-    trip_id: "JFH367",
-    arrival_time: "07:25:00",
-    departure_time: "07:25:00",
-    stop_id: "bUCR-0-05",
-    stop_sequence: 17,
-    stop_headsign: "Facultad de Ingeniería",
-    pickup_type: 0,
-    drop_off_type: 0,
-    shape_dist_traveled: 1.5
-  },
-  {
-    trip_id: "APV3225",
-    arrival_time: "07:25:00",
-    departure_time: "07:25:00",
-    stop_id: "bUCR-0-05",
-    stop_sequence: 17,
-    stop_headsign: "Facultad de Ingeniería",
-    pickup_type: 0,
-    drop_off_type: 0,
-    shape_dist_traveled: 1.5
-  }
-]
+const { getStopTimes } = useStopTimes()
 
 async function fetchStopTimes() {
   if (!tripId.value) {
-    // Return all entries if no Trip ID is entered
-    tableData.value = mockStopTimes
+    tableData.value = getStopTimes()
     output.value = ''
     return
   }
 
   try {
-    const data = mockStopTimes.filter(entry => entry.trip_id === tripId.value)
+    const data = getStopTimes(tripId.value)
     tableData.value = data
     output.value = data.length === 0 ? 'No results found.' : ''
   } catch (e: any) {
@@ -116,6 +70,7 @@ async function fetchStopTimes() {
   }
 }
 </script>
+
 
 <style scoped>
 .config-form {
